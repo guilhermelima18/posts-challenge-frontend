7365de4d74c87d251952eed6645130b97f96854c
guilhermelima18 <guilhermelima18@hotmail.com>
2023-07-24 17:30:25 -0300

    feat: update to createPostSchema

--- src/validations/createPostSchema.ts
+++ src/validations/createPostSchema.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const createPostSchema = yup.object({
-  title: yup.string().required(),
-  body: yup.string().required(),
+  title: yup.string().required("Título é obrigatório."),
+  body: yup.string().required("Esse campo é obrigatório."),
 });
